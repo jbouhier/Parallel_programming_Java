1405c42c2ee25f102221fedf46f7916ea03fbdef
Jean-Baptiste Bouhier <jb.bouhier@gmail.com>
2017-08-08 10:18:42 -0700

    Implemented parMatrixMultiply() with PCDP API

--- 1-parallel_programming_in_java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
+++ 1-parallel_programming_in_java/miniproject_3/src/main/java/edu/coursera/parallel/MatrixMultiply.java
@@ -1,5 +1,6 @@
 package edu.coursera.parallel;
 
+import static edu.rice.pcdp.PCDP.forall2dChunked;
 import static edu.rice.pcdp.PCDP.forseq2d;
 
 /**
@@ -20,8 +21,7 @@ public final class MatrixMultiply {
      * @param C The output matrix
      * @param N Size of each dimension of the input matrices
      */
-    public static void seqMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
+    public static void seqMatrixMultiply(final double[][] A, final double[][] B, final double[][] C, final int N) {
         forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
@@ -38,13 +38,8 @@ public final class MatrixMultiply {
      * @param C The output matrix
      * @param N Size of each dimension of the input matrices
      */
-    public static void parMatrixMultiply(final double[][] A, final double[][] B,
-            final double[][] C, final int N) {
-        /*
-         * TODO Parallelize this outermost two-dimension sequential loop to
-         * achieve performance improvement.
-         */
-        forseq2d(0, N - 1, 0, N - 1, (i, j) -> {
+    public static void parMatrixMultiply(final double[][] A, final double[][] B, final double[][] C, final int N) {
+        forall2dChunked(0, N - 1, 0, N - 1, (i, j) -> {
             C[i][j] = 0.0;
             for (int k = 0; k < N; k++) {
                 C[i][j] += A[i][k] * B[k][j];
